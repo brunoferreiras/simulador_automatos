3efffd3c236c45456a18c69318356720ebb62dfb
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-06-14 14:07:18 +0000

    Iniciando o trabalho com a implementação do layout

---
+++ app/Http/Controllers/AutomatoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Automato;
+use Illuminate\Http\Request;
+
+class AutomatoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Automato  $automato
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Automato $automato)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Automato  $automato
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Automato $automato)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Automato  $automato
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Automato $automato)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Automato  $automato
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Automato $automato)
+    {
+        //
+    }
+}

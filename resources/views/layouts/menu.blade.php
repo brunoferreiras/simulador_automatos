f4f3b2fd64fe512d9777aaeba153b4bca5387450
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-06-15 17:32:45 -0300

    Teste com o springyjs

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -71,6 +71,13 @@
 	    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	    <!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		{{-- ArborJS --}}
+        {{--<script src="{{ asset('/js/arborjs/lib/arbor.js') }}"></script>--}}
+        {{--<script src="{{ asset('/js/arborjs/lib/arbor-tween.js') }}"></script>--}}
+        <script src="{{ asset('/js/springy-master/springy.js') }}"></script>
+        <script src="{{ asset('/js/springy-master/springyui.js') }}"></script>
+
+        @yield('scripts')
 
         {{-- PNotify --}}
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.2.0/pnotify.js"></script>
--- resources/views/operacoes/grafico.blade.php
+++ resources/views/operacoes/grafico.blade.php
@@ -1,2 +1,28 @@
-<?php
-        echo "TESTE";
+@extends('layouts.menu')
+
+@section('content')
+    <div class="col-sm-12">
+        <canvas id="springydemo" width="800" height="480"></canvas>
+    </div>
+@endsection
+@section('scripts')
+    <script>
+        $(function() {
+            var graph = new Springy.Graph();
+            var layout = new Springy.Layout.ForceDirected(graph, 400.0, 400.0, 0.5);
+
+            graph.addNodes('X1', 'X2');
+
+            graph.addEdges(
+                ['X1', 'X2', {color: '#00A0B0', label: 'Foo bar'}],
+                ['X2', 'X1', {color: '#b06a61', label: 'Foo bar'}]
+            );
+
+            jQuery(function(){
+                var springy = jQuery('#springydemo').springy({
+                    graph: graph
+                });
+            });
+        });
+    </script>
+@endsection

66d34b2961afe4b08b1d7821620a15a3d92d8333
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-06-16 03:02:47 +0000

    Adicionado as funções para gerar os estados marcados e o estado inicial

--- resources/views/operacoes/grafico.blade.php
+++ resources/views/operacoes/grafico.blade.php
@@ -5,11 +5,7 @@
     <script src="http://visjs.org/dist/vis.js"></script>
     <link href="http://visjs.org/dist/vis-network.min.css" rel="stylesheet" type="text/css" />
     <style type="text/css">
-        html, body {
-            font: 10pt arial;
-        }
-
-        #mynetwork {
+         #myautomato {
             width: 100%;
             height: 600px;
             border: 1px solid lightgray;
@@ -18,28 +14,30 @@
 </head>
 
 <body>
-<div id="mynetwork"></div>
+<div id="myautomato"></div>
 
 <script type="text/javascript">
     var nodes = new vis.DataSet([
-        {id: 1, label: '0'},
-        {id: 2, label: '1'},
-        {id: 3, label: '2'},
-        {id: 4, label: '3'}
+        {id: 'start', label: 'Estado Inicial', font: {size: 14}},
+        {id: 0, label: '0'},
+        {id: 1, label: '1'},
+        {id: 2, label: '2'},
+        {id: 3, label: '3'}
     ]);
 
     // create an array with edges
     var edges = new vis.DataSet([
-        {from: 1, to: 1, label: 'a'},
-        {from: 1, to: 2, label: 'b'},
-        {from: 2, to: 1, label: 'a'},
-        {from: 2, to: 4, label: 'b'},
-        {from: 2, to: 3, label: 'c'},
-        {from: 4, to: 1, label: 'a'}
+        {from: 'start', to: 0},
+        {from: 0, to: 0, label: 'a'},
+        {from: 0, to: 1, label: 'b'},
+        {from: 1, to: 0, label: 'a'},
+        {from: 1, to: 3, label: 'b'},
+        {from: 1, to: 2, label: 'c'},
+        {from: 3, to: 0, label: 'a'}
     ]);
 
     // create a network
-    var container = document.getElementById('mynetwork');
+    var container = document.getElementById('myautomato');
     var data = {
         nodes: nodes,
         edges: edges
@@ -91,6 +89,17 @@
         }
     };
     var network = new vis.Network(container, data, options);
+
+    // Onde vai ser definido os estados marcados
+    network.on("afterDrawing", function (ctx) {
+        var nodeId = 0;
+        var nodePosition = network.getPositions([nodeId]);
+        ctx.strokeStyle = '#343434';
+        ctx.lineWidth = 2;
+        ctx.fillStyle = '#A6D5F7';
+        ctx.circle(nodePosition[nodeId].x, nodePosition[nodeId].y,18);
+        ctx.stroke();
+    });
 </script>
 </body>
 </html>

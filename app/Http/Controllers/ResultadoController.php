1d57e9da01e0b9204b76ef36091c9c786482a278
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-06-16 21:07:11 +0000

    Resultado da funçãoiniciado

--- app/Http/Controllers/ResultadoController.php
+++ app/Http/Controllers/ResultadoController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Automato;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\FuncaoController;
 class ResultadoController extends Controller
 {
+    private $automato;
 
     public function resultadoFuncao(Request $request)
     {
+        $automato = Automato::find($request->automato);
+        $this->automato = new FuncaoController($automato);
+        dd($this->automato);
+        $funcao = $request->funcao;
+
+
         $title = "TESTE";
         $possuiGrafico = true;
         return view('resultados.funcao', compact('title', 'possuiGrafico'));
